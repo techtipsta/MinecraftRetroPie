539f89eb0e3c27218e156d430d6f481f3ad206dd
Scottt <scott.geyer@eagles.usm.edu>
2017-02-23 01:57:23 +0000

    Update MinecraftRetroInstaller.sh

--- MinecraftRetroInstaller.sh
+++ MinecraftRetroInstaller.sh
@@ -6,34 +6,34 @@ sudo apt-get -y install matchbox
 
 #create folders for install
 
-sudo mkdir /home/pi/RetroPie/roms/minecraft
+sudo mkdir /home/pi/RetroPie/roms/scratch
 
 #copy appropriate script files
 
-cd /home/pi/RetroPie/roms/minecraft/
+cd /home/pi/RetroPie/roms/scratch
 #sudo wget https://github.com/techtipsta/MinecraftRetroPie/raw/master/minecraft2.6.sh
-sudo wget https://github.com/techtipsta/MinecraftRetroPie/raw/master/minecraft.sh
+sudo wget https://github.com/punkbass/ScratchRetroPie/raw/master/scratch.sh
 
 
 #Install Minecraft to Emulation Station as its own device menu
 
-sudo sed -i -e 's|</systemList>|<system>\n<name>minecraft</name>\n<fullname>Minecraft</fullname>\n<path>~/RetroPie/roms/minecraft</path>\n<extension>.sh .SH</extension>\n<command>xinit /home/pi/RetroPie/roms/minecraft/minecraft.sh</command>\n<platform>minecraft</platform>\n<theme>minecraft</theme>\n</system>\n</systemList>|g' /etc/emulationstation/es_systems.cfg
-sudo mkdir /etc/emulationstation/themes/simple/minecraft
-sudo mkdir /etc/emulationstation/themes/simple/minecraft/art/
+sudo sed -i -e 's|</systemList>|<system>\n<name>scratch</name>\n<fullname>Scratch</fullname>\n<path>~/RetroPie/roms/scratch</path>\n<extension>.sh .SH</extension>\n<command>xinit /home/pi/RetroPie/roms/scratch/scratch.sh</command>\n<platform>scratch</platform>\n<theme>scratch</theme>\n</system>\n</systemList>|g' /etc/emulationstation/es_systems.cfg
+sudo mkdir /etc/emulationstation/themes/simple/scratch
+sudo mkdir /etc/emulationstation/themes/simple/scratch/art/
 
-cd /etc/emulationstation/themes/simple/minecraft/
-sudo wget https://github.com/techtipsta/MinecraftRetroPie/raw/master/minecraftmenu/theme.xml
+cd /etc/emulationstation/themes/simple/scratch/
+sudo wget https://github.com/punkbass/ScratchRetroPie/raw/master/scratchmenu/theme.xml
 
-cd /etc/emulationstation/themes/simple/minecraft/art/
-sudo wget https://github.com/techtipsta/MinecraftRetroPie/raw/master/minecraftmenu/art/minecraft.png
-sudo wget https://github.com/techtipsta/MinecraftRetroPie/raw/master/minecraftmenu/art/minecraft_art.png
-sudo wget https://github.com/techtipsta/MinecraftRetroPie/raw/master/minecraftmenu/art/minecraft_art_blur.png
+cd /etc/emulationstation/themes/simple/scratch/art/
+sudo wget https://github.com/punkbass/ScratchRetroPie/raw/master/scratchmenu/art/scratch.png
+sudo wget https://github.com/punkbass/ScratchRetroPie/raw/master/scratchmenu/art/scratch_art.png
+sudo wget https://github.com/punkbass/ScratchRetroPie/raw/master/scratchmenu/art/scratch_art_blur.png
 
-cd /home/pi/RetroPie/roms/minecraft
+cd /home/pi/RetroPie/roms/scratch
 
 #Chmod scripts to be runnable
 
-#chmod +x minecraft2.6.sh
-chmod +x minecraft.sh
+#chmod +x scratch.sh
+chmod +x scratch.sh
 
 echo -e "\nEverything done! \n"
