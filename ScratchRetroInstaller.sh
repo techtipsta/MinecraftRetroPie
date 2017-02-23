3117a946dcf81f4f6f7fb2f56918fa23faef85fd
Scottt <scott.geyer@eagles.usm.edu>
2017-02-22 20:42:06 -0600

    Update ScratchRetroInstaller.sh

--- ScratchRetroInstaller.sh
+++ ScratchRetroInstaller.sh
@@ -15,7 +15,7 @@ cd /home/pi/RetroPie/roms/scratch
 sudo wget https://github.com/punkbass/ScratchRetroPie/raw/master/scratch.sh
 
 
-#Install Minecraft to Emulation Station as its own device menu
+#Install Scratch to Emulation Station as its own device menu
 
 sudo sed -i -e 's|</systemList>|<system>\n<name>scratch</name>\n<fullname>Scratch</fullname>\n<path>~/RetroPie/roms/scratch</path>\n<extension>.sh .SH</extension>\n<command>xinit /home/pi/RetroPie/roms/scratch/scratch.sh</command>\n<platform>scratch</platform>\n<theme>scratch</theme>\n</system>\n</systemList>|g' /etc/emulationstation/es_systems.cfg
 sudo mkdir /etc/emulationstation/themes/simple/scratch
