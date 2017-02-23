790cdbb02921f9a759ca3beb486c835885a8d0e9
Scottt <scott.geyer@eagles.usm.edu>
2017-02-22 21:17:35 -0600

    Update scratch.sh

--- scratch.sh
+++ scratch.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-xset -dpms  # disable DPMS (Energy Star) features.
-xset s off  # disable screen saver
-xset s noblank  # don't blank the video device
-matchbox-window-manager & scratch
+#xset -dpms  # disable DPMS (Energy Star) features.
+#xset s off  # disable screen saver
+#xset s noblank  # don't blank the video device
+#matchbox-window-manager & scratch
+scratch
